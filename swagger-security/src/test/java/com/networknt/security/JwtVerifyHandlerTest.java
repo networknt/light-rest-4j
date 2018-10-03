00d2804db1d08e10432caeeffd6120f6409e70dc
Steve Hu <stevehu@gmail.com>
2018-10-02 21:12:54 -0400

    fixes #59 update default validator.yml and security.yml to framework specific

--- swagger-security/src/test/java/com/networknt/security/JwtVerifyHandlerTest.java
+++ swagger-security/src/test/java/com/networknt/security/JwtVerifyHandlerTest.java
@@ -107,7 +107,7 @@ public class JwtVerifyHandlerTest {
         final CountDownLatch latch = new CountDownLatch(1);
         final ClientConnection connection;
         try {
-            connection = client.connect(new URI("http://localhost:8080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.POOL, OptionMap.EMPTY).get();
+            connection = client.connect(new URI("http://localhost:8080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
         } catch (Exception e) {
             throw new ClientException(e);
         }
@@ -137,7 +137,7 @@ public class JwtVerifyHandlerTest {
         final CountDownLatch latch = new CountDownLatch(1);
         final ClientConnection connection;
         try {
-            connection = client.connect(new URI("http://localhost:8080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.POOL, OptionMap.EMPTY).get();
+            connection = client.connect(new URI("http://localhost:8080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
         } catch (Exception e) {
             throw new ClientException(e);
         }
