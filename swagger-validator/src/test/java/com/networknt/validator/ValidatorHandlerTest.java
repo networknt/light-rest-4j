ae6de8dc78f6326b09d2265bfaff486a02c3f8d6
ddobrin <dan.dpobrin@gmail.com>
2018-10-04 09:23:50 -0400

    Fixes #60 - Added a test case for descriptions returned with escaped characters

--- swagger-validator/src/test/java/com/networknt/validator/ValidatorHandlerTest.java
+++ swagger-validator/src/test/java/com/networknt/validator/ValidatorHandlerTest.java
@@ -305,6 +305,44 @@ public class ValidatorHandlerTest {
             Assert.assertEquals("addPet", body);
         }
     }
+    
+    @Test
+    public void testInvalidPostWithEscapeResponse() throws Exception {
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
+        try {
+            connection = client.connect(new URI("http://localhost:8080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+
+        try {
+        	String post = "{\"id\":0,\"shipDate\":\"2001-01-0167\"}";
+        	connection.getIoThread().execute(new Runnable() {
+                @Override
+                public void run() {
+                    final ClientRequest request = new ClientRequest().setMethod(Methods.POST).setPath("/v2/store/order");
+                    request.getRequestHeaders().put(Headers.HOST, "localhost");
+                    request.getRequestHeaders().put(Headers.CONTENT_TYPE, "application/json");
+                    request.getRequestHeaders().put(Headers.TRANSFER_ENCODING, "chunked");
+                    connection.sendRequest(request, client.createClientCallback(reference, latch, post));
+                }
+            });
+
+            latch.await(10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            logger.error("IOException: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        int statusCode = reference.get().getResponseCode();
+        String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+        Assert.assertEquals(400, statusCode);
+        Assert.assertTrue(body.contains("ERR11004"));
+    }    
 
     @Test
     public void testGetParam() throws Exception {
