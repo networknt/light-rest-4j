cf41b292c0c6a9d61bcab574772d546e9661fd76
Steve Hu <stevehu@gmail.com>
2019-08-19 17:45:00 -0400

    fixes #103 replace JwtHelper with JwtVerifier to remove dependency to security.yml

--- openapi-security/src/main/java/com/networknt/openapi/JwtVerifyHandler.java
+++ openapi-security/src/main/java/com/networknt/openapi/JwtVerifyHandler.java
@@ -25,8 +25,9 @@ import com.networknt.oas.model.Operation;
 import com.networknt.oas.model.Path;
 import com.networknt.oas.model.SecurityParameter;
 import com.networknt.oas.model.SecurityRequirement;
-import com.networknt.security.JwtHelper;
+import com.networknt.security.IJwtVerifyHandler;
 import com.networknt.exception.ExpiredTokenException;
+import com.networknt.security.JwtVerifier;
 import com.networknt.utility.Constants;
 import com.networknt.utility.ModuleRegistry;
 import io.undertow.Handlers;
@@ -49,7 +50,7 @@ import java.util.*;
  *
  * @author Steve Hu
  */
-public class JwtVerifyHandler implements MiddlewareHandler {
+public class JwtVerifyHandler implements MiddlewareHandler, IJwtVerifyHandler {
     static final Logger logger = LoggerFactory.getLogger(JwtVerifyHandler.class);
 
     static final String OPENAPI_SECURITY_CONFIG = "openapi-security";
@@ -66,11 +67,14 @@ public class JwtVerifyHandler implements MiddlewareHandler {
     static final String STATUS_METHOD_NOT_ALLOWED = "ERR10008";
 
     static Map<String, Object> config;
+    // make this static variable public so that it can be accessed from the server-info module
+    public static JwtVerifier jwtVerifier;
     static {
         // check if openapi-security.yml exist
         config = Config.getInstance().getJsonMapConfig(OPENAPI_SECURITY_CONFIG);
         // fallback to generic security.yml
-        if(config == null) config = Config.getInstance().getJsonMapConfig(JwtHelper.SECURITY_CONFIG);
+        if(config == null) config = Config.getInstance().getJsonMapConfig(JwtVerifier.SECURITY_CONFIG);
+        jwtVerifier = new JwtVerifier(config);
     }
 
     private volatile HttpHandler next;
@@ -81,10 +85,10 @@ public class JwtVerifyHandler implements MiddlewareHandler {
     public void handleRequest(final HttpServerExchange exchange) throws Exception {
         HeaderMap headerMap = exchange.getRequestHeaders();
         String authorization = headerMap.getFirst(Headers.AUTHORIZATION);
-        String jwt = JwtHelper.getJwtFromAuthorization(authorization);
+        String jwt = jwtVerifier.getJwtFromAuthorization(authorization);
         if(jwt != null) {
             try {
-                JwtClaims claims = JwtHelper.verifyJwt(jwt, false);
+                JwtClaims claims = jwtVerifier.verifyJwt(jwt, false, true);
                 Map<String, Object> auditInfo = exchange.getAttachment(AuditHandler.AUDIT_INFO);
                 // In normal case, the auditInfo shouldn't be null as it is created by OpenApiHandler with
                 // endpoint and swaggerOperation available. This handler will enrich the auditInfo.
@@ -125,11 +129,11 @@ public class JwtVerifyHandler implements MiddlewareHandler {
 
                     // is there a scope token
                     String scopeHeader = headerMap.getFirst(HttpStringConstants.SCOPE_TOKEN);
-                    String scopeJwt = JwtHelper.getJwtFromAuthorization(scopeHeader);
+                    String scopeJwt = jwtVerifier.getJwtFromAuthorization(scopeHeader);
                     List<String> secondaryScopes = null;
                     if(scopeJwt != null) {
                         try {
-                            JwtClaims scopeClaims = JwtHelper.verifyJwt(scopeJwt, false);
+                            JwtClaims scopeClaims = jwtVerifier.verifyJwt(scopeJwt, false, true);
                             secondaryScopes = scopeClaims.getStringListClaimValue("scope");
                             auditInfo.put(Constants.SCOPE_CLIENT_ID_STRING, scopeClaims.getStringClaimValue(Constants.CLIENT_ID_STRING));
                             auditInfo.put(Constants.ACCESS_CLAIMS, scopeClaims);
@@ -222,7 +226,7 @@ public class JwtVerifyHandler implements MiddlewareHandler {
 
     @Override
     public boolean isEnabled() {
-        Object object = config.get(JwtHelper.ENABLE_VERIFY_JWT);
+        Object object = config.get(JwtVerifier.ENABLE_VERIFY_JWT);
         return object != null && Boolean.valueOf(object.toString()) ;
     }
 
@@ -231,4 +235,8 @@ public class JwtVerifyHandler implements MiddlewareHandler {
         ModuleRegistry.registerModule(JwtVerifyHandler.class.getName(), config, null);
     }
 
+    @Override
+    public JwtVerifier getJwtVerifier() {
+        return jwtVerifier;
+    }
 }
