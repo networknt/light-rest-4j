70b9210d2692afc1539c5aa8637e86ce31ab35ae
chenyan71 <gavinyan.han@gmail.com>
2019-11-14 09:45:04 -0500

    fixes #117

--- openapi-validator/src/main/java/com/networknt/openapi/ValidatorHandler.java
+++ openapi-validator/src/main/java/com/networknt/openapi/ValidatorHandler.java
@@ -82,16 +82,13 @@ public class ValidatorHandler implements MiddlewareHandler {
             setExchangeStatus(exchange, STATUS_MISSING_OPENAPI_OPERATION);
             return;
         }
-        String contentType = exchange.getRequestHeaders().getFirst(Headers.CONTENT_TYPE);
-        if (contentType==null || contentType.startsWith("application/json")) {
-            Status status = requestValidator.validateRequest(requestPath, exchange, openApiOperation);
-            if(status != null) {
-                exchange.setStatusCode(status.getStatusCode());
-                status.setDescription(status.getDescription().replaceAll("\\\\", "\\\\\\\\"));
-                exchange.getResponseSender().send(status.toString());
-                if(config.logError) logger.error("ValidationError:" + status.toString());
-                return;
-            }
+        Status status = requestValidator.validateRequest(requestPath, exchange, openApiOperation);
+        if(status != null) {
+            exchange.setStatusCode(status.getStatusCode());
+            status.setDescription(status.getDescription().replaceAll("\\\\", "\\\\\\\\"));
+            exchange.getResponseSender().send(status.toString());
+            if(config.logError) logger.error("ValidationError:" + status.toString());
+            return;
         }
 
         if(config.validateResponse) {
