a3a69f45531e8100829d4f9995d2eb02c5d7ac5a
Steve Hu <stevehu@gmail.com>
2020-11-23 11:57:28 -0500

    fixes #150 support cid uid for client_id and user_id for some jwt tokens

--- openapi-security/src/main/java/com/networknt/openapi/JwtVerifyHandler.java
+++ openapi-security/src/main/java/com/networknt/openapi/JwtVerifyHandler.java
@@ -97,8 +97,14 @@ public class JwtVerifyHandler implements MiddlewareHandler, IJwtVerifyHandler {
                     auditInfo = new HashMap<>();
                     exchange.putAttachment(AttachmentConstants.AUDIT_INFO, auditInfo);
                 }
-                auditInfo.put(Constants.CLIENT_ID_STRING, claims.getStringClaimValue(Constants.CLIENT_ID_STRING));
-                auditInfo.put(Constants.USER_ID_STRING, claims.getStringClaimValue(Constants.USER_ID_STRING));
+                String clientId = claims.getStringClaimValue(Constants.CLIENT_ID_STRING);
+                // try to get the cid as some OAuth tokens name it as cid like Okta.
+                if(clientId == null) clientId = claims.getStringClaimValue(Constants.CID_STRING);
+                auditInfo.put(Constants.CLIENT_ID_STRING, clientId);
+                String userId = claims.getStringClaimValue(Constants.USER_ID_STRING);
+                // try to get the uid as some OAuth tokens name it as uid like Okta.
+                if(userId == null) userId = claims.getStringClaimValue(Constants.UID_STRING);
+                auditInfo.put(Constants.USER_ID_STRING, userId);
                 auditInfo.put(Constants.SUBJECT_CLAIMS, claims);
                 String callerId = headerMap.getFirst(HttpStringConstants.CALLER_ID);
                 if(callerId != null) auditInfo.put(Constants.CALLER_ID_STRING, callerId);
