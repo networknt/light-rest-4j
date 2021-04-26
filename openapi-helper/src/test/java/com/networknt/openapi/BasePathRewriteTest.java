8b0104cb13fede610df38fddf1d33233788f19ab
Bolun Wen <whenballoon@gmail.com>
2021-04-26 13:12:58 -0400

    issue-#178 - enhanced tests - fixed openapi was cached in different test classes

--- openapi-helper/src/test/java/com/networknt/openapi/BasePathRewriteTest.java
+++ openapi-helper/src/test/java/com/networknt/openapi/BasePathRewriteTest.java
@@ -1,16 +1,27 @@
 package com.networknt.openapi;
 
 import com.networknt.config.Config;
+import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-public class BasePathTest {
+import java.lang.reflect.Field;
+
+public class BasePathRewriteTest {
     @Before
     public void testOAuth2Name() {
-        String spec = Config.getInstance().getStringFromFile("openapi-relative-server-url.yaml");
+        String spec = Config.getInstance().getStringFromFile("openapi-server-url-rewrite.yaml");
         OpenApiHelper.init(spec);
     }
+
+    @After
+    public void tearDown() throws NoSuchFieldException, IllegalAccessException {
+        Field instance = OpenApiHelper.class.getDeclaredField("INSTANCE");
+        instance.setAccessible(true);
+        instance.set(null, null);
+    }
+
     @Test
     public void testBasePath() {
         Assert.assertEquals("/namespace/application/v1", OpenApiHelper.basePath);
