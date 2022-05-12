0c7cbd88ceaca253f14efbfd81b70a36002e3c41
Steve Hu <stevehu@gmail.com>
2022-05-12 13:28:11 -0400

    fixes #205 update specui and add favicon handler to enhance the swagger ui rendering

--- specification/src/main/java/com/networknt/specification/SpecDisplayHandler.java
+++ specification/src/main/java/com/networknt/specification/SpecDisplayHandler.java
@@ -27,14 +27,12 @@ import io.undertow.util.HttpString;
  * @author Gavin Chen
  */
 public class SpecDisplayHandler implements LightHttpHandler {
-    public static final String CONFIG_NAME = "specification";
-
     public SpecDisplayHandler(){
     }
 
     @Override
     public void handleRequest(HttpServerExchange exchange) throws Exception {
-        SpecificationConfig config = (SpecificationConfig)Config.getInstance().getJsonObjectConfig(CONFIG_NAME, SpecificationConfig.class);
+        SpecificationConfig config = (SpecificationConfig)Config.getInstance().getJsonObjectConfig(SpecificationConfig.CONFIG_NAME, SpecificationConfig.class);
         final String payload = Config.getInstance().getStringFromFile(config.getFileName());
         exchange.getResponseHeaders().add(new HttpString("Content-Type"), config.getContentType());
         exchange.getResponseSender().send(payload);
---
+++ specification/src/test/java/com/networknt/specification/SpecSwaggerUIHandlerTest.java
@@ -0,0 +1,90 @@
+package com.networknt.specification;
+
+import com.networknt.client.Http2Client;
+import com.networknt.exception.ClientException;
+import io.undertow.Handlers;
+import io.undertow.Undertow;
+import io.undertow.client.ClientConnection;
+import io.undertow.client.ClientRequest;
+import io.undertow.client.ClientResponse;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.RoutingHandler;
+import io.undertow.util.Headers;
+import io.undertow.util.Methods;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xnio.IoUtils;
+import org.xnio.OptionMap;
+
+import java.net.URI;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class SpecSwaggerUIHandlerTest {
+    static final Logger logger = LoggerFactory.getLogger(SpecSwaggerUIHandlerTest.class);
+
+    static Undertow server = null;
+
+    @BeforeClass
+    public static void setUp() {
+        if(server == null) {
+            logger.info("starting server");
+            HttpHandler handler = getTestHandler();
+            server = Undertow.builder()
+                    .addHttpListener(7080, "localhost")
+                    .setHandler(handler)
+                    .build();
+            server.start();
+        }
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        if(server != null) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ignored) {
+
+            }
+            server.stop();
+            logger.info("The server is stopped.");
+        }
+    }
+
+    static RoutingHandler getTestHandler() {
+        return Handlers.routing().add(Methods.GET, "/specui.html", new SpecSwaggerUIHandler());
+    }
+
+    @Test
+    public void testSpecUI() throws Exception {
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
+        try {
+            connection = client.connect(new URI("http://localhost:7080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        try {
+            ClientRequest request = new ClientRequest().setPath("/specui.html").setMethod(Methods.GET);
+            request.getRequestHeaders().put(Headers.HOST, "localhost");
+            connection.sendRequest(request, client.createClientCallback(reference, latch));
+            latch.await();
+        } catch (Exception e) {
+            logger.error("Exception: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        int statusCode = reference.get().getResponseCode();
+        String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+        Assert.assertEquals(200, statusCode);
+        Assert.assertNotNull( body);
+    }
+
+}
