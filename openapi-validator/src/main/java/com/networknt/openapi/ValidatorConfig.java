c2b147be5aeedf606542b746ad88e701cb563060
Steve Hu <stevehu@gmail.com>
2022-11-18 23:07:14 -0500

    fixes #250 refactor openapi-validator module to support reload and skipPathPrefixes

--- openapi-validator/src/main/java/com/networknt/openapi/ValidatorConfig.java
+++ openapi-validator/src/main/java/com/networknt/openapi/ValidatorConfig.java
@@ -15,19 +15,71 @@
  */
 package com.networknt.openapi;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.networknt.config.Config;
+import com.networknt.config.ConfigException;
+import com.networknt.security.SecurityConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 /**
  * Validator configuration class that maps to validator.yml properties
  *
  * @author Steve Hu
  */
 public class ValidatorConfig {
+    public static final String CONFIG_NAME = "openapi-validator";
+    private static final Logger logger = LoggerFactory.getLogger(ValidatorConfig.class);
+    private static final String ENABLED = "enabled";
+    private static final String LOG_ERROR = "logError";
+    private static final String SKIP_BODY_VALIDATION = "skipBodyValidation";
+    private static final String VALIDATE_RESPONSE = "validateResponse";
+    private static final String HANDLE_NULLABLE_FIELD = "handleNullableField";
+    private static final String SKIP_PATH_PREFIXES = "skipPathPrefixes";
+
+    private Map<String, Object> mappedConfig;
+    private Config config;
+
     boolean enabled;
     boolean logError;
     boolean skipBodyValidation = false;
     boolean validateResponse;
     boolean handleNullableField = true;
+    private List<String> skipPathPrefixes;
 
-    public ValidatorConfig() {
+    private ValidatorConfig(String configName) {
+        config = Config.getInstance();
+        mappedConfig = config.getJsonMapConfigNoCache(configName);
+        setConfigData();
+        setConfigList();
+    }
+    private ValidatorConfig() {
+        this(CONFIG_NAME);
+    }
+
+    public static ValidatorConfig load(String configName) {
+        return new ValidatorConfig(configName);
+    }
+
+    public static ValidatorConfig load() {
+        return new ValidatorConfig();
+    }
+
+    public void reload() {
+        mappedConfig = config.getJsonMapConfigNoCache(CONFIG_NAME);
+        setConfigData();
+        setConfigList();
+    }
+
+    public void reload(String configName) {
+        mappedConfig = config.getJsonMapConfigNoCache(configName);
+        setConfigData();
+        setConfigList();
     }
 
     public boolean isEnabled() {
@@ -65,4 +117,76 @@ public class ValidatorConfig {
 	public void setHandleNullableField(boolean handleNullableField) {
 		this.handleNullableField = handleNullableField;
 	}
+
+    public List<String> getSkipPathPrefixes() {
+        return skipPathPrefixes;
+    }
+
+    public void setSkipPathPrefixes(List<String> skipPathPrefixes) {
+        this.skipPathPrefixes = skipPathPrefixes;
+    }
+
+    public Map<String, Object> getMappedConfig() {
+        return mappedConfig;
+    }
+
+    Config getConfig() {
+        return config;
+    }
+
+    private void setConfigData() {
+        if(getMappedConfig() != null) {
+            Object object = getMappedConfig().get(ENABLED);
+            if(object != null && (Boolean) object) {
+                enabled = true;
+            }
+            object = getMappedConfig().get(LOG_ERROR);
+            if(object != null && (Boolean) object) {
+                logError = true;
+            }
+            object = getMappedConfig().get(SKIP_BODY_VALIDATION);
+            if(object != null && (Boolean) object) {
+                skipBodyValidation = true;
+            }
+            object = getMappedConfig().get(VALIDATE_RESPONSE);
+            if(object != null && (Boolean) object) {
+                validateResponse = true;
+            }
+            object = getMappedConfig().get(HANDLE_NULLABLE_FIELD);
+            if(object != null && (Boolean) object) {
+                handleNullableField = true;
+            }
+        }
+    }
+
+    private void setConfigList() {
+        if (mappedConfig != null && mappedConfig.get(SKIP_PATH_PREFIXES) != null) {
+            Object object = mappedConfig.get(SKIP_PATH_PREFIXES);
+            skipPathPrefixes = new ArrayList<>();
+            if(object instanceof String) {
+                String s = (String)object;
+                s = s.trim();
+                if(logger.isTraceEnabled()) logger.trace("s = " + s);
+                if(s.startsWith("[")) {
+                    // json format
+                    try {
+                        skipPathPrefixes = Config.getInstance().getMapper().readValue(s, new TypeReference<List<String>>() {});
+                    } catch (Exception e) {
+                        throw new ConfigException("could not parse the skipPathPrefixes json with a list of strings.");
+                    }
+                } else {
+                    // comma separated
+                    skipPathPrefixes = Arrays.asList(s.split("\\s*,\\s*"));
+                }
+            } else if (object instanceof List) {
+                List prefixes = (List)object;
+                prefixes.forEach(item -> {
+                    skipPathPrefixes.add((String)item);
+                });
+            } else {
+                throw new ConfigException("skipPathPrefixes must be a string or a list of strings.");
+            }
+        }
+    }
+
 }
