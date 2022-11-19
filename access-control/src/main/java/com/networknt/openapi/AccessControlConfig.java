f8f85c834e7251cba8b2a6ed8d005957630b6a28
Steve Hu <stevehu@gmail.com>
2022-11-18 22:59:59 -0500

    fixes #244 update access-control to refactor the config class and add skipPathPrefixes (#245)

--- access-control/src/main/java/com/networknt/openapi/AccessControlConfig.java
+++ access-control/src/main/java/com/networknt/openapi/AccessControlConfig.java
@@ -15,13 +15,53 @@
  */
 package com.networknt.openapi;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.networknt.config.Config;
+import com.networknt.config.ConfigException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
 class AccessControlConfig {
+    private static final Logger logger = LoggerFactory.getLogger(AccessControlConfig.class);
     public static final String CONFIG_NAME = "access-control";
+    private static final String ENABLED = "enabled";
+    private static final String DEFAULT_DENY = "defaultDeny";
+    private static final String SKIP_PATH_PREFIXES = "skipPathPrefixes";
+
+    private Map<String, Object> mappedConfig;
+    private Config config;
+
     boolean enabled;
     boolean defaultDeny;
+    private List<String> skipPathPrefixes;
+
 
     private AccessControlConfig() {
+        this(CONFIG_NAME);
+    }
+    private AccessControlConfig(String configName) {
+        config = Config.getInstance();
+        mappedConfig = config.getJsonMapConfigNoCache(configName);
+        setConfigData();
+        setConfigList();
+    }
 
+    public static AccessControlConfig load() {
+        return new AccessControlConfig(CONFIG_NAME);
+    }
+    public static AccessControlConfig load(String configName) {
+        return new AccessControlConfig(configName);
+    }
+
+    public void reload() {
+        mappedConfig = config.getJsonMapConfigNoCache(CONFIG_NAME);
+        setConfigData();
+        setConfigList();
     }
 
     public boolean isEnabled() {
@@ -39,4 +79,62 @@ class AccessControlConfig {
     public void setDefaultDeny(boolean defaultDeny) {
         this.defaultDeny = defaultDeny;
     }
+
+    public List<String> getSkipPathPrefixes() {
+        return skipPathPrefixes;
+    }
+
+    public void setSkipPathPrefixes(List<String> skipPathPrefixes) {
+        this.skipPathPrefixes = skipPathPrefixes;
+    }
+    public Map<String, Object> getMappedConfig() {
+        return mappedConfig;
+    }
+    Config getConfig() {
+        return config;
+    }
+
+    private void setConfigData() {
+        if(getMappedConfig() != null) {
+            Object object = getMappedConfig().get(ENABLED);
+            if(object != null && (Boolean) object) {
+                enabled = true;
+            }
+            object = getMappedConfig().get(DEFAULT_DENY);
+            if(object != null && (Boolean) object) {
+                defaultDeny = true;
+            }
+        }
+    }
+
+    private void setConfigList() {
+        if (mappedConfig != null && mappedConfig.get(SKIP_PATH_PREFIXES) != null) {
+            Object object = mappedConfig.get(SKIP_PATH_PREFIXES);
+            skipPathPrefixes = new ArrayList<>();
+            if(object instanceof String) {
+                String s = (String)object;
+                s = s.trim();
+                if(logger.isTraceEnabled()) logger.trace("s = " + s);
+                if(s.startsWith("[")) {
+                    // json format
+                    try {
+                        skipPathPrefixes = Config.getInstance().getMapper().readValue(s, new TypeReference<List<String>>() {});
+                    } catch (Exception e) {
+                        throw new ConfigException("could not parse the skipPathPrefixes json with a list of strings.");
+                    }
+                } else {
+                    // comma separated
+                    skipPathPrefixes = Arrays.asList(s.split("\\s*,\\s*"));
+                }
+            } else if (object instanceof List) {
+                List prefixes = (List)object;
+                prefixes.forEach(item -> {
+                    skipPathPrefixes.add((String)item);
+                });
+            } else {
+                throw new ConfigException("skipPathPrefixes must be a string or a list of strings.");
+            }
+        }
+    }
+
 }
