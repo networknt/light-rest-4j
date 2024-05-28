599e334ccbab144e783e15de5ceffcbcce6dd99f
Steve Hu <stevehu@gmail.com>
2024-05-28 16:43:45 -0400

    fixes #368 Add a test case and configurations to test enum validation (#369)

---
+++ openapi-validator/src/test/java/com/networknt/openapi/ValidatorHandlerEnumTest.java
@@ -0,0 +1,266 @@
+package com.networknt.openapi;
+
+import com.networknt.body.BodyHandler;
+import com.networknt.client.Http2Client;
+import com.networknt.config.Config;
+import com.networknt.exception.ClientException;
+import com.networknt.status.Status;
+import io.undertow.Handlers;
+import io.undertow.Undertow;
+import io.undertow.client.ClientConnection;
+import io.undertow.client.ClientRequest;
+import io.undertow.client.ClientResponse;
+import io.undertow.server.HttpHandler;
+import io.undertow.server.RoutingHandler;
+import io.undertow.util.Headers;
+import io.undertow.util.HttpString;
+import io.undertow.util.Methods;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xnio.IoUtils;
+import org.xnio.OptionMap;
+
+import java.io.*;
+import java.net.URI;
+import java.util.Map;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ValidatorHandlerEnumTest {
+    static final Logger logger = LoggerFactory.getLogger(ValidatorHandlerEnumTest.class);
+
+    static Undertow server = null;
+
+    static Map<String, Object> responses = Config.getInstance().getJsonMapConfig("responses");
+
+    static String logFile = "target/test.log";
+
+    File f = new File(logFile);
+
+    @Before
+    public void setUp() {
+        if(server == null) {
+            logger.info("starting server");
+            OpenApiHandlerConfig openApiHandlerConfig = OpenApiHandlerConfig.load("openapi-handler-enum");
+            OpenApiHandler openApiHandler = new OpenApiHandler(openApiHandlerConfig);
+            BodyHandler bodyHandler = new BodyHandler();
+
+            HttpHandler handler = getPetStoreHandler();
+            ValidatorHandler validatorHandler = new ValidatorHandler();
+            validatorHandler.setNext(handler);
+            handler = validatorHandler;
+
+            bodyHandler.setNext(handler);
+            handler = bodyHandler;
+
+            openApiHandler.setNext(handler);
+            handler = openApiHandler;
+
+            server = Undertow.builder()
+                    .addHttpListener(7080, "localhost")
+                    .setHandler(handler)
+                    .build();
+            server.start();
+            clearLogFile();
+        }
+    }
+
+    @Before
+    public void clearLogFile() {
+        synchronized (f) {
+            try {
+                FileWriter fw = new FileWriter(f, false);
+                fw.write("");
+                fw.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @AfterClass
+    public static void tearDown() throws Exception {
+        if(server != null) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ignored) {
+
+            }
+            server.stop();
+            logger.info("The server is stopped.");
+        }
+    }
+
+
+    RoutingHandler getPetStoreHandler() {
+        ForwardRequestHandler forwardHandler = new ForwardRequestHandler();
+        return Handlers.routing()
+                .add(Methods.POST, "/v1/pets", exchange -> exchange.getResponseSender().send("addPet"))
+                .add(Methods.GET, "/v1/pets/{petId}", exchange -> exchange.getResponseSender().send("getPetById"))
+                .add(Methods.DELETE, "/v1/pets/{petId}", exchange -> exchange.getResponseSender().send("deletePetById"))
+                .add(Methods.GET, "/v1/todoItems", forwardHandler)
+                .add(Methods.GET, "/v1/pets", exchange -> {
+                    if (exchange.getQueryParameters() != null && exchange.getQueryParameters().containsKey("arr")) {
+                        exchange.getResponseSender().send("getPets" + ", the query parameter = " + exchange.getQueryParameters() + ", length = " + exchange.getQueryParameters().get("arr").size());
+                    } else {
+                        exchange.getResponseSender().send("getPets");
+                    }
+                });
+    }
+
+    @Test
+    public void testValidPostEnum() throws Exception {
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
+        try {
+            connection = client.connect(new URI("http://localhost:7080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+
+        try {
+            String post = "{\"id\":1,\"name\":\"doggie\"}";
+            connection.getIoThread().execute(new Runnable() {
+                @Override
+                public void run() {
+                    final ClientRequest request = new ClientRequest().setMethod(Methods.POST).setPath("/v1/pets");
+                    request.getRequestHeaders().put(Headers.HOST, "localhost");
+                    request.getRequestHeaders().put(Headers.CONTENT_TYPE, "application/json");
+                    request.getRequestHeaders().put(Headers.TRANSFER_ENCODING, "chunked");
+                    request.getRequestHeaders().put(new HttpString("accessId"), "001");
+                    request.getRequestHeaders().put(new HttpString("requestId"), "64");
+                    connection.sendRequest(request, client.createClientCallback(reference, latch, post));
+                }
+            });
+
+            latch.await(10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            logger.error("IOException: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        int statusCode = reference.get().getResponseCode();
+        String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+        Assert.assertEquals(200, statusCode);
+        if(statusCode == 200) {
+            Assert.assertNotNull(body);
+            Assert.assertEquals("addPet", body);
+        }
+    }
+
+
+    @Test
+    public void testInValidPostEnum() throws Exception {
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
+        try {
+            connection = client.connect(new URI("http://localhost:7080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+
+        try {
+            String post = "{\"id\":0,\"name\":\"doggie\"}";
+            connection.getIoThread().execute(new Runnable() {
+                @Override
+                public void run() {
+                    final ClientRequest request = new ClientRequest().setMethod(Methods.POST).setPath("/v1/pets");
+                    request.getRequestHeaders().put(Headers.HOST, "localhost");
+                    request.getRequestHeaders().put(Headers.CONTENT_TYPE, "application/json");
+                    request.getRequestHeaders().put(Headers.TRANSFER_ENCODING, "chunked");
+                    request.getRequestHeaders().put(new HttpString("accessId"), "001");
+                    request.getRequestHeaders().put(new HttpString("requestId"), "64");
+                    connection.sendRequest(request, client.createClientCallback(reference, latch, post));
+                }
+            });
+
+            latch.await(10, TimeUnit.SECONDS);
+        } catch (Exception e) {
+            logger.error("IOException: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        int statusCode = reference.get().getResponseCode();
+        String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+        Assert.assertEquals(400, statusCode);
+        if(statusCode == 400) {
+            Status status = Config.getInstance().getMapper().readValue(body, Status.class);
+            Assert.assertNotNull(status);
+            Assert.assertEquals("ERR11004", status.getCode());
+        }
+    }
+
+    @Test
+    public void testGetValidPathParam() throws Exception {
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
+        try {
+            connection = client.connect(new URI("http://localhost:7080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        try {
+            ClientRequest request = new ClientRequest().setPath("/v1/pets/def").setMethod(Methods.GET);
+            request.getRequestHeaders().put(Headers.HOST, "localhost");
+            connection.sendRequest(request, client.createClientCallback(reference, latch));
+            latch.await();
+        } catch (Exception e) {
+            logger.error("Exception: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        int statusCode = reference.get().getResponseCode();
+        String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+        Assert.assertEquals(200, statusCode);
+        if(statusCode == 200) {
+            Assert.assertNotNull(body);
+            Assert.assertEquals("getPetById", body);
+        }
+    }
+
+    @Test
+    public void testGetInValidPathParam() throws Exception {
+        final Http2Client client = Http2Client.getInstance();
+        final CountDownLatch latch = new CountDownLatch(1);
+        final ClientConnection connection;
+        try {
+            connection = client.connect(new URI("http://localhost:7080"), Http2Client.WORKER, Http2Client.SSL, Http2Client.BUFFER_POOL, OptionMap.EMPTY).get();
+        } catch (Exception e) {
+            throw new ClientException(e);
+        }
+        final AtomicReference<ClientResponse> reference = new AtomicReference<>();
+        try {
+            ClientRequest request = new ClientRequest().setPath("/v1/pets/111").setMethod(Methods.GET);
+            request.getRequestHeaders().put(Headers.HOST, "localhost");
+            connection.sendRequest(request, client.createClientCallback(reference, latch));
+            latch.await();
+        } catch (Exception e) {
+            logger.error("Exception: ", e);
+            throw new ClientException(e);
+        } finally {
+            IoUtils.safeClose(connection);
+        }
+        int statusCode = reference.get().getResponseCode();
+        String body = reference.get().getAttachment(Http2Client.RESPONSE_BODY);
+        Assert.assertEquals(400, statusCode);
+        if(statusCode == 400) {
+            Status status = Config.getInstance().getMapper().readValue(body, Status.class);
+            Assert.assertNotNull(status);
+            Assert.assertEquals("ERR11004", status.getCode());
+        }
+    }
+
+}
